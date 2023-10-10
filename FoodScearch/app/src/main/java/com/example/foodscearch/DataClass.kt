9a3b5c92bcc036c2547f52eb134710cb7df0ecbc
bosingyim <114340148+bosingyim@users.noreply.github.com>
2023-10-10 19:26:54 +0700

    Add files via upload

---
+++ FoodScearch/app/src/main/java/com/example/foodscearch/DataClass.kt
@@ -0,0 +1,3 @@
+package com.example.foodscearch
+
+data class DataClass(var dataImage:Int, var dataTitle:String)
---
+++ FoodScearch/app/src/main/java/com/example/foodscearch/MainActivity.kt
@@ -0,0 +1,44 @@
+package com.example.foodscearch
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+class MainActivity : AppCompatActivity() {
+
+
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var dataList: ArrayList<DataClass>
+    lateinit var imageList:Array<Int>
+    lateinit var titleList:Array<String>
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        imageList = arrayOf(
+            R.drawable.ic_lab)
+
+        titleList = arrayOf(
+            "Lab")
+
+
+        recyclerView = findViewById(R.id.recyclerView)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.setHasFixedSize(true)
+
+        dataList = arrayListOf<DataClass>()
+        getData()
+
+
+    }
+
+    private fun getData(){
+        for (i in imageList.indices){
+            val dataClass = DataClass(imageList[i], titleList[i])
+            dataList.add(dataClass)
+        }
+        recyclerView.adapter = AdapterClass(dataList)
+    }
+}
